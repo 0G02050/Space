e68c23f60ea5309e76fde48af0596dce398ef1f7
リハクエン <wuy2752@gmail.com>
2025-12-24 00:34:08 +0900

    Initial Commit

---
+++ Space/SpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceApp.swift
+//  Space
+//
+//  Created by 小川正博 on 2025/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
